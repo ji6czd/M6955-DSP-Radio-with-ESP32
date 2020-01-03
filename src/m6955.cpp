25f5ead4356c26150ae20a96edfb0969ce20b33a
KIRIAKE Masanori <seiken@rd01.net>
2020-01-03 13:24:04 +0900

    Add: 	soundOut class. Change: 	いろいろ。

--- src/m6955.cpp
+++ src/m6955.cpp
@@ -1,56 +1,163 @@
 #include <arduino.h>
 #include <Wire.h>
 #include <m6955.h>
-
-#define AKC6955_ADDR 0x10
+// Power on line GPIO
 #define P_ON 23
+// I2C address
+#define AKC6955_ADDR 0x10
+// Register definitions.
+#define AKC6955_CONFIG  0
+#define AKC6955_BAND   1
+#define AKC6955_CH_HI  2
+#define AKC6955_CH_LO  3
+#define AKC6955_UCH_ST 4
+#define AKC6955_UCH_EN 5
+#define AKC6955_VOLUME 6
+#define AKC6955_STEREO 7
+#define AKC6955_THRESH 8
+#define AKC6955_ENABLE 9
+#define AKC6955_FM_SEEKSTEP 10
+#define AKC6955_SPACE  11
+#define AKC6955_ADCEN 12
+#define AKC6955_PRE 13
+// 14-19 = RESERVE
+
+// Read only registers
+#define AKC6955_RCH_HI   20
+#define AKC6955_RCH_LO   21
+#define AKC6955_CNR_AM   22
+#define AKC6955_CNR_FM   23
+#define AKC6955_PGALEVEL 24
+#define AKC6955_VBAT     25
+#define AKC6955_FDNUM    26
+#define AKC6955_RSSI     27
+
+struct config_bits {
+	uint8_t reservee:2;
+	uint8_t mute:1;
+	uint8_t seekup:1;
+	uint8_t seek:1;
+	uint8_t tune:1;
+	uint8_t fm_en:1;
+	uint8_t power:1;
+};
+
+union akc6955Config {
+	uint8_t byte;
+	config_bits bits;
+};
+
+M6955 radio;
 
 static bool SetupI2C()
 {
   pinMode(21, INPUT_PULLUP);
   pinMode(22, INPUT_PULLUP);
-  Wire.begin(21,22);
-  Wire.setClock(400000);
-  return true;
+  bool ret = Wire.begin(21,22, 400000);
+  return ret;
 }
 
-uint8_t i2c_write(uint8_t device_address, uint8_t memory_address, uint8_t value)
+uint8_t m6955Write(uint8_t memory_address, uint8_t value)
 {
-  Wire.beginTransmission(device_address);
+  Wire.beginTransmission(AKC6955_ADDR);
   Wire.write(memory_address);
   Wire.write(value);
   uint8_t ret = Wire.endTransmission();
-  delay(3);
 	return ret;
 }
 
-uint8_t i2c_read(uint8_t device_address, uint8_t memory_address)
+uint8_t m6955Read(uint8_t memory_address)
 {
-  uint8_t data;
-  Wire.beginTransmission(device_address);
+  uint8_t data=0;
+  Wire.beginTransmission(AKC6955_ADDR);
   Wire.write(memory_address);
   Wire.endTransmission(false);
-  Wire.requestFrom(device_address, 1, false);
-  data = Wire.read();
-  Wire.endTransmission(true);
-  delay(2);
-  return data;
+	delay(1);
+	Wire.requestFrom(AKC6955_ADDR, 1);
+	delay(1);
+	if (Wire.available()) {
+		data = Wire.read();
+	}
+	Wire.endTransmission(1);
+	return data;
 }
 
 /* class functions */
 bool M6955::begin()
 {
-  SetupI2C();
+	if (!SetupI2C()) {
+		error = 'I';
+		return false;
+	}
+	return true;
 }
 
 bool M6955::powerOn()
 {
   pinMode(P_ON, OUTPUT);
   digitalWrite(P_ON, HIGH);
-	delay(3);
-	if (i2c_write(AKC6955_ADDR, 0x00, 0b10000000)) {
+	delay(0);
+	akc6955Config cfg;
+	 cfg.byte = m6955Read(AKC6955_CONFIG);
+	 Serial.println(cfg.byte, BIN);
+	cfg.bits.mute=0; // cleaer mute bit
+	cfg.bits.power=1; // Set power flag
+	if (m6955Write(AKC6955_CONFIG, cfg.byte)) {
 		// Power onに失敗してる
+		error = 'P';
 		return false;
 	}
   return true;
+}
+
+bool M6955::powerOff()
+{
+  pinMode(P_ON, OUTPUT);
+  digitalWrite(P_ON, HIGH);
+	delay(0);
+	akc6955Config cfg;
+	 cfg.byte = m6955Read(AKC6955_CONFIG);
+	 Serial.println(cfg.byte, BIN);
+	cfg.bits.mute=0; // cleaer mute bit
+	cfg.bits.power=0; // Set power flag
+	if (m6955Write(AKC6955_CONFIG, cfg.byte)) {
+		// Power offに失敗してる
+		error = 'P';
+		return false;
+	}
+  return true;
+}
+
+uint8_t M6955::getBand(void)
+{
+	return m6955Read(AKC6955_BAND);
+}
+
+uint16_t M6955::getCh(void)
+{
+	uint16_t ch = m6955Read(AKC6955_CH_HI);
+	ch  = ch << 8;
+	ch += m6955Read(AKC6955_CH_LO);
+	return ch;
+}
+
+bool M6955::getMode()
+{
+	akc6955Config cfg;
+	cfg.byte = m6955Read(AKC6955_CONFIG);
+	return cfg.bits.fm_en;
+}
+
+bool M6955::setMode(bool mode)
+{
+	akc6955Config cfg;
+	cfg.byte = m6955Read(AKC6955_CONFIG);
+	cfg.bits.fm_en = mode;
+	m6955Write(AKC6955_CONFIG, cfg.byte);
+	return cfg.bits.fm_en;
+}
+
+bool M6955::setBand(uint8_t band)
+{
+	return true;
 }
--- src/main.ino
+++ src/main.ino
@@ -1,17 +1,57 @@
 #include <arduino.h>
 #include <Wire.h>
 #include <m6955.h>
+#include <soundOut.h>
+
+void swSetup()
+{
+	pinMode(13, INPUT_PULLUP);
+	pinMode(12, INPUT_PULLUP);
+}
 
 void setup()
 {
+	sOut.begin();
+	sOut.morseOut('s');
 	Serial.begin(115200);
 	Serial.println("Hello M6955");
-	radio.begin();
+	swSetup();
+	if (!radio.begin()) {
+		sOut.morseOut(radio.errorStatus());
+		Serial.println("I2C initialize error.");
+	}
 	if (!radio.powerOn()) {
 		Serial.println("Power on failed");
+		sOut.morseOut(radio.errorStatus());
 	}
+}
+
+bool readSw(uint8_t sw)
+{
+	if (!digitalRead(sw)) {
+		// キーが押された
+		while(!digitalRead(sw))
+			; // Waiting pin is high
+		delay(50); // この間はキーを検出しない
+	} else {
+		return false;
+	}
+	return true;
 }
 
 void loop()
 {
+	bool ret = false;
+	if (readSw(13)) {
+		radio.powerOff();
+		Serial.println("13 ON");
+	}
+	else if (readSw(12)) {
+		radio.powerOn();
+		Serial.println("12 ON");
+	}
+	else if (readSw(14)) {
+		Serial.println(radio.getBand(), BIN);
+		Serial.println("14 ON");
+	}
 }
