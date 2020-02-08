e2414d7e409e27b9cd12b1a4f137e12e233acb1e
KIRIAKE Masanori <seiken@rd01.net>
2020-02-09 06:48:01 +0900

    Just commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
 
 extern "C" {
   int app_main()
