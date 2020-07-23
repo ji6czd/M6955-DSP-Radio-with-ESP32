24ba6f8f3b143374d9197453ae7086add046f040
KIRIAKE Masanori <seiken@rd01.net>
2020-07-24 01:24:13 +0900

    Changed: 	* 初期化関数で入力を切り替えられるようにしてみた。

--- components/ES8388/include/ES8388.hxx
+++ components/ES8388/include/ES8388.hxx
@@ -1,12 +1,18 @@
 #ifndef _ES8388_H_
 #define _ES8388_H_
+
+enum Input_t {
+	      In1,
+	      In2,
+	      Dac
+};
+
 class ES8388 {
 public:
   int init();
+  int selectSrc(Input_t src);
   int readAll();
 private:
-  int initNormalMode();
-  int initBypassMode();
 };
 extern ES8388 es8388;
 #endif
